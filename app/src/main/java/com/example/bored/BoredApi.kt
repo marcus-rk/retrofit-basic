af7d4ae3104c05a9ceb20996e78d2e9ec1451022
marcus_rk <makj0005@stud.kea.dk>
2024-04-11 10:35:59 +0200

    Started working on Exercise C

--- app/src/main/java/com/example/bored/BoredApi.kt
+++ app/src/main/java/com/example/bored/BoredApi.kt
@@ -1,7 +1,8 @@
 package com.example.bored
 
+import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BoredApi {
@@ -13,4 +14,11 @@ interface BoredApi {
     suspend fun getEventByParticipants(
         @Query("participants") participants: Int
     ): RandomEvent
+
+    @GET("/api/activity")
+    suspend fun getEventByAccessibilityAndPriceRange(
+        @Query("accessibility") accessibility: Double,
+        @Query("minprice") minprice: Double,
+        @Query("maxprice") maxprice: Double,
+    ): Call<ResponseBody>
 }
--- app/src/main/java/com/example/bored/app.kt
+++ app/src/main/java/com/example/bored/app.kt
@@ -1,6 +1,7 @@
 package com.example.bored
 
 import kotlinx.coroutines.runBlocking
+import retrofit2.awaitResponse
 
 fun main() {
     runBlocking {
@@ -13,5 +14,35 @@ fun main() {
         val participantsInput = readlnOrNull()?.toInt()
         val factByParticipants = instance.apiService.getEventByParticipants(participantsInput!!)
         println(factByParticipants)
+
+        println("\nRandom fact by accessibility and price-range")
+        print("Insert accessibility: ")
+        val accessibilityInput = readlnOrNull()?.toDouble()
+        print("Insert min-price: ")
+        val minPriceInput = readlnOrNull()?.toDouble()
+        print("Insert max-price: ")
+        val maxPriceInput = readlnOrNull()?.toDouble()
+
+        try {
+            val factByAccessibilityAndPriceRangeResponse = instance.apiService.getEventByAccessibilityAndPriceRange(
+                accessibilityInput!!,
+                minPriceInput!!,
+                maxPriceInput!!
+            ).awaitResponse()
+
+            println(factByAccessibilityAndPriceRangeResponse.toString())
+            println(factByAccessibilityAndPriceRangeResponse.message())
+            println(factByAccessibilityAndPriceRangeResponse.isSuccessful)
+
+            if (factByAccessibilityAndPriceRangeResponse.isSuccessful) {
+                println("Success!")
+                println(factByAccessibilityAndPriceRangeResponse.body()?.string())
+            } else {
+                println("Failure")
+            }
+        } catch (e: Exception) {
+            println("Request failed: ${e.message}")
+        }
+
     }
 }
